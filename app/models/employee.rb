1732003d82d67eac86c32fe8cbd72fd2ba64a257
shenye76 <shinyainoue76@gmail.com>
2020-01-19 22:13:04 +0900

    フリガナ入力のバリデーション

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,7 +2,10 @@ class Employee < ApplicationRecord
   belongs_to :company
   mount_uploader :image, ImageUploader
 
-  enum sex: [:"男", :"女"]
-  enum pay_type: [:"月給", :"時給", :"日給", :"その他"]
+  validates :family_name_f,:first_name_f, presence: true, format: { with: /[\u{ff66}-\u{ff9f}]/, \
+            message: 'には日本語を含めてください。(Text must contain katakana.)' }
+
+  enum sex: {"男":0, "女":1}
+  enum pay_type: {"月給":0, "時給":1, "日給":2, "その他":3}
   
 end
