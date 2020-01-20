270b3942ac2b6da7faaf8f1eb815a99c129d24bb
shenye76 <shinyainoue76@gmail.com>
2020-01-20 19:34:20 +0900

    rootes.rbの修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,7 @@ Rails.application.routes.draw do
     registrations: 'companies/registrations',
     sessions: "companies/sessions",
   }
-<<<<<<< Updated upstream
-  root 'companies#edit' #後ほど変更する
-=======
   root 'companies#index'
->>>>>>> Stashed changes
   resources :companies, only: [:edit, :update]
 
 end
